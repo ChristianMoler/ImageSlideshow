8179c1e4ee3cab1c73ee7bdda4d331f7b6be6806
Moler Christian <moler@reksoft.ru>
2021-12-20 16:15:40 +0300

    remove unused dependencies

--- Package.swift
+++ Package.swift
@@ -12,18 +12,10 @@ let package = Package(
             name: "ImageSlideshow",
             targets: ["ImageSlideshow"]),
         .library(
-            name: "ImageSlideshow/Alamofire",
-            targets: ["ImageSlideshowAlamofire"]),
-        .library(
             name: "ImageSlideshow/SDWebImage",
-            targets: ["ImageSlideshowSDWebImage"]),
-        .library(
-            name: "ImageSlideshow/Kingfisher",
-            targets: ["ImageSlideshowKingfisher"])
+            targets: ["ImageSlideshowSDWebImage"])
     ],
     dependencies: [
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "5.8.0"),
-        .package(url: "https://github.com/Alamofire/AlamofireImage.git", from: "4.0.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")
     ],
     targets: [
@@ -47,22 +39,12 @@ let package = Package(
             resources: [
                 .copy("Assets/ic_cross_white@2x.png"),
                 .copy("Assets/ic_cross_white@3x.png"),
-            ]),
-        .target(
-            name: "ImageSlideshowAlamofire",
-            dependencies: ["ImageSlideshow", "AlamofireImage"],
-            path: "ImageSlideshow/Classes/InputSources",
-            sources: ["AlamofireSource.swift"]),
+            ])
         .target(
             name: "ImageSlideshowSDWebImage",
             dependencies: ["ImageSlideshow", "SDWebImage"],
             path: "ImageSlideshow/Classes/InputSources",
-            sources: ["SDWebImageSource.swift"]),
-        .target(
-            name: "ImageSlideshowKingfisher",
-            dependencies: ["ImageSlideshow", "Kingfisher"],
-            path: "ImageSlideshow/Classes/InputSources",
-            sources: ["KingfisherSource.swift"])
+            sources: ["SDWebImageSource.swift"])
     ],
     swiftLanguageVersions: [.v4, .v4_2, .v5]
 )
